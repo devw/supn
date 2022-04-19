48293a676eff6b10a2a4b820daf12a20e8f90a5a
Antonio Pierro <antonio.pierro@gmail.com>
2022-04-19 11:22:48 +0200

    feat: add exercises

--- projects/test-graphical-representation.py
+++ projects/test-graphical-representation.py
@@ -6,21 +6,21 @@ import unittest
 
 
 def get_plot_x_y():
-    # Write a function called get_plot_x_y that returns the matplotlib.pyplot instance whitout showing the plot
+    # Write a function called get_plot_x_y that returns a matplotlib.pyplot instance that plots a list of (x,y) coordinates.
     # The function must have two parameters.
     # The first parameter represents the x-coordinate
     # The second parameter represents the y-coordinate
-    # TODO
+    plt.show(block=False)
     return plt
 
 
 def get_plot_x_y_title():
-    # Write a function called get_plot_x_y_title that returns the matplotlib.pyplot instance whitout showing the plot
+    # Write a function called get_plot_x_y_title that returns a matplotlib.pyplot instance that plots a list of (x,y) coordinates and has a title
     # The function must have three parameters.
     # The 1st parameter represents the x-coordinate of the matplotlib.pyplot instance
     # The 2nd parameter represents the y-coordinate of the matplotlib.pyplot instance
     # The 3rd parameter represents the plot title of the matplotlib.pyplot instance
-    # TODO
+    plt.show(block=False)
     return plt
 
 ### Testing ### 
@@ -35,6 +35,7 @@ class TestGraphicalInterface(unittest.TestCase):
         line = gca.lines[0]
         self.assertTrue((line.get_xdata() == x).all())
         self.assertTrue((line.get_ydata() == y).all())
+        plt.show()
 
     def test_get_plot_x_y_title(self):
         x = [1, 2, 5]
@@ -45,6 +46,7 @@ class TestGraphicalInterface(unittest.TestCase):
         self.assertTrue((line.get_xdata() == x).all())
         self.assertTrue((line.get_ydata() == y).all())
         self.assertTrue((gca.get_title() == title))
+        plt.show()
 
 
 if __name__ == '__main__':
